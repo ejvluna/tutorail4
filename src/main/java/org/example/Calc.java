4b77eacf02f6cb56b7451dc6f11b33c84e6d01aa
ejvluna <137990447+ejvluna@users.noreply.github.com>
2024-08-16 16:53:56 +1200

    Fix subtract method in Calc class

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -6,9 +6,10 @@ public class Calc {
     {
         return x+y;
     }
-
+    // Modify the subtract method in the Calc class to perform subtraction correctly
     public int subtract(int x, int y)
     {
-        return x*y;
+        return x-y;
     }
+    // {return x*y;}
 }
